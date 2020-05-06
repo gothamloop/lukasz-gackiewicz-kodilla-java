fe951f101ac51f6ed41bbbf7c9059893bd99dced
gothamloop <g.otham@webtunnelo.com>
2020-05-06 11:41:10 +0200

    First Commit

--- kodilla-testing/src/main/java/TestingMain.java
+++ kodilla-testing/src/main/java/TestingMain.java
@@ -1,7 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+
+        System.out.println("Rozpoczynam test klasy Calkulator:");
+
+        if(Calculator.result_addSum == 9){
+            System.out.println("Dodawanie działa poprawnie");
+        } else {
+            System.out.println("Dodawanie działa niepoprawnie");
+        }
+
+        if(Calculator.result_substractSum == 3){
+            System.out.println("Odejmowanie działa poprawnie");
+        } else {
+            System.out.println("Odejmowanie działa niepoprawnie");
+        }
+
+
+
+
+
+
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.calculator;
+
+public class Calculator {
+
+    public static int result_addSum;
+    public static int result_substractSum;
+
+    public int add(int a, int b){
+        int result_addSum = add(4,5);
+        return result_addSum;
+    }
+
+    public int substract(int a, int b){
+        int result_substractSum = substract(10,7);
+        return result_substractSum;
+    }
+}
