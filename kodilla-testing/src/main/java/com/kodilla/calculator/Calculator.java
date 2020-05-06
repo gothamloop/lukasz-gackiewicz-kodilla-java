80dd894d83bac7c3ee44a6b1740b129ca6908fab
gothamloop <g.otham@webtunnelo.com>
2020-05-06 13:24:53 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/calculator/Calculator.java
@@ -6,12 +6,12 @@ public class Calculator {
     public static int result_subSum;
 
     public int add(int a, int b){
-     //   result_addSum = a + b;
+        result_addSum = a + b;
         return result_addSum;
     }
 
     public int subtract(int a, int b){
-     //   result_subSum = a -b;
+        result_subSum = a -b;
        return result_subSum;
     }
 }
