c5c159c194e9f515fd23d5db01de19c81f9828dd
gothamloop <g.otham@webtunnelo.com>
2020-05-11 17:03:22 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CountStatistics.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/CountStatistics.java
@@ -1,4 +1,57 @@
 package com.kodilla.testing.forum.statistics;
 
 public class CountStatistics {
+
+    private int numberOfUsers;
+    private int numberOfPosts;
+    private int numberOfComments;
+    private int avgPostsPerUser;
+    private int avgCommentsPerUser;
+    private int avgCommentsPerPost;
+
+
+    public int getNumberOfUsersUsers() {
+        return numberOfUsers;
+    }
+
+    public int getNumberOfPosts() {
+        return numberOfPosts;
+    }
+
+    public int getNumberOfComments() {
+        return numberOfComments;
+    }
+
+    public int getAvgPostsPerUser() {
+        return avgPostsPerUser;
+    }
+
+    public int getAvgCommentsPerUser() {
+        return avgCommentsPerUser;
+    }
+
+    public int getAvgCommentsPerPost() {
+        return avgCommentsPerPost;
+    }
+
+
+
+   private void calculateAdvStatistics(Statistics statistics){
+       numberOfUsers
+
+
+
+
+
+
+   }
+
+
+
+
+   private void showStatistics(){
+
+   }
+
+
 }
