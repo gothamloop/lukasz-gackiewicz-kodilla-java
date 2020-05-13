b7e7dbd766b19fcbfd54e017356f7ee8b2f50cda
gothamloop <g.otham@webtunnelo.com>
2020-05-13 07:46:57 +0200

    Commit First

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuite.java
@@ -31,39 +31,53 @@ public class StatisticsTestSuite {
         assertEquals(0, CountStatistics.getNumberOfPosts());
     }
 
-  /*  @Test
+    @Test
     public void testStatisticsPostsEquals1000(){
         Statistics statisticsMock = mock(Statistics.class);
         when(statisticsMock.postsCount()).thenReturn(1000);
         CountStatistics.calculateAdvStatistics(statisticsMock);
-        assertEquals(1000, CountStatistics.getNumberOfPosts());
+        assertEquals(1000, CountStatistics.postsCount());
 
-    } */
+    }
 
     @Test
     public void testStatisticsCommentsEqualsZero(){
-
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+        assertEquals(0, CountStatistics.commentsCount());
     }
 
     @Test
     public void testStatisticsCommentsMoreThanPosts(){
-
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(5);
+        when(statisticsMock.postsCount()).thenReturn(3);
+        assertEquals(5, CountStatistics.commentsCount());
     }
 
     @Test
     public void testStatisticsCommentsLowerThanPosts(){
-
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.commentsCount()).thenReturn(5);
+        when(statisticsMock.postsCount()).thenReturn(12);
+        assertEquals(5, CountStatistics.commentsCount());
     }
 
     @Test
-    public void testStatisticsNumberofUsersEqualsZero(){
-
+    public void testStatisticsNumberOfUsersEqualsZero(){
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.usersNames().size()).thenReturn(0);
+        CountStatistics.calculateAdvStatistics(statisticsMock);
+        assertEquals(0, CountStatistics.statistics.usersNames().size());
     }
 
 
     @Test
     public void testStatisticsNumberofUsersEquals100(){
-
+        Statistics statisticsMock = mock(Statistics.class);
+        when(statisticsMock.usersNames().size()).thenReturn(100);
+        CountStatistics.calculateAdvStatistics(statisticsMock);
+        assertEquals(0, CountStatistics.statistics.usersNames().size());
     }
 
 
