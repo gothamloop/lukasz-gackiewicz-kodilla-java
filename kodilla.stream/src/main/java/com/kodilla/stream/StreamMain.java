eefee2cadc2b508f8dd546b71be513fe81809783
gothamloop <g.otham@webtunnelo.com>
2020-05-14 13:54:39 +0200

    First Commit

--- kodilla.stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla.stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,16 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.person.People;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.book.Book;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Map;
+
+
+
+
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -18,7 +28,6 @@ public class StreamMain {
           expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
           expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
           expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
 
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         String beautifulUpiekszacz1 = poemBeautifier.beautify("Beautiful text example no 1", (text -> text.toUpperCase()));
@@ -36,7 +45,78 @@ public class StreamMain {
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
+
+
+        People.getList().stream()
+                .forEach(System.out::println);
+
+        People.getList().stream()
+                .map(s -> s.toUpperCase())
+                .forEach(System.out::println);
+
+        People.getList().stream()
+                .filter(s -> s.length() > 11)
+                .forEach(System.out::println);
+
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+
+
+        BookDirectory theBookDirectory = new BookDirectory();
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
 */
+
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));
+
+        System.out.println(theResultStringOfBooks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
