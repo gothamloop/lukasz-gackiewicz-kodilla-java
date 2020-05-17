7b6fcb9addac01d7652bb3bc63b9c4be6dc29550
gothamloop <g.otham@webtunnelo.com>
2020-05-17 19:32:31 +0200

    First Commit

---
+++ kodilla.stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Continent {
+
+
+    private final List<Country> listOfCountries = new ArrayList<>();
+    private final String nameOfContinent;
+
+
+    public Continent(String nameOfContinent) {
+        this.nameOfContinent = nameOfContinent;
+    }
+
+    public List<Country> getListOfCountries() {
+        return listOfCountries;
+    }
+
+    public String getNameOfContinent() {
+        return nameOfContinent;
+    }
+}
---
+++ kodilla.stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,25 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class World {
+
+    private final List<Continent> listOfContinents = new ArrayList<>();
+
+    public List<Continent> getListOfContinents() {
+        return listOfContinents;
+    }
+
+    public BigDecimal getPeopleQuantity() {
+
+        BigDecimal peopleQuantityOnTheWorld = listOfContinents.stream()
+                .flatMap(continent -> continent.getListOfCountries().stream())
+                .map(Country::getPeopleQuantity)
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
+
+        return peopleQuantityOnTheWorld;
+    }
+
+}
