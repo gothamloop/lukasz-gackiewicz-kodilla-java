8f067a0957b690f352e4b1ed1712b26d8b4c636f
gothamloop <g.otham@webtunnelo.com>
2020-05-18 11:36:44 +0200

    First Commit

--- kodilla.stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla.stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -7,14 +7,16 @@ public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
 
-        IntStream.range(0, numbers.size())
+        IntStream.range(0, numbers.length)
                 .map(n->numbers[n])
                 .forEach(System.out::println);
 
 
-        return IntStream.range(0, numbers.size())
+        IntStream.range(0, numbers.length)
                 .map(n->numbers[n])
                 .average()
                 .getAsDouble();
+
+
     }
 }
