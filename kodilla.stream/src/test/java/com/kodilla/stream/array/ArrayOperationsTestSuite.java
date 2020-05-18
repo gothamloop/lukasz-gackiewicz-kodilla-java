f6fe1f0257a2564f44f331dc199bafb6bee9ed12
gothamloop <g.otham@webtunnelo.com>
2020-05-18 11:18:56 +0200

    First Commit

---
+++ kodilla.stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+
+    @Test
+    public void testGetAverage(){
+
+        int[] numbers = new int[20];
+        numbers[0] = 2;
+        numbers[1] = 54;
+        numbers[2] = 6;
+        numbers[3] = 41;
+        numbers[4] = 7;
+        numbers[5] = 16;
+        numbers[6] = 14;
+        numbers[7] = 92;
+        numbers[8] = 10;
+        numbers[9] = 87;
+        numbers[10] = 3;
+        numbers[11] = 22;
+        numbers[12] = 6;
+        numbers[13] = 8;
+        numbers[14] = 5;
+        numbers[15] = 7;
+        numbers[16] = 14;
+        numbers[17] = 8;
+        numbers[18] = 15;
+        numbers[19] = 11;
+
+
+     // sum = 428
+     // average = 21.4
+
+        double avgShouldBe = 21.4;
+        double avgCountedFromNumbers = ArrayOperations.getAverage(numbers);
+        Assert.assertEquals(avgShouldBe, avgCountedFromNumbers, 0.0001);
+
+    }
+}
