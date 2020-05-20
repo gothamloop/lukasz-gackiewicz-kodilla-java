363054d09d4b956a01d2aa8cc40ff8c37f59f41e
gothamloop <g.otham@webtunnelo.com>
2020-05-20 18:22:03 +0200

    First Commit

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -11,6 +11,7 @@ public class FileReader {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
 
+    //    try (Stream<String> fileLines = Files.lines(Paths.get("test.txt"))) {
         try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
