5a6fbf4e8699f2551fb769770d42c88a4e872797
gothamloop <g.otham@webtunnelo.com>
2020-05-20 18:58:51 +0200

    First Commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,8 +2,8 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
 
-    public double divide(double a, double b) throws ArithmeticException {
-        if(b == 0){
+    public static double divide(double a, double b) throws ArithmeticException {
+        if(b == 0 || a == 0){
             throw new ArithmeticException();
         }
         return a / b;
@@ -15,11 +15,19 @@ public class FirstChallenge {
      */
     public static void main(String[] args) {
 
-        FirstChallenge firstChallenge = new FirstChallenge();
-
+     /*   FirstChallenge firstChallenge = new FirstChallenge();
         double result = firstChallenge.divide(3, 0);
-
         System.out.println(result);
-
+*/
+        double result=0;
+        try{
+            result = FirstChallenge.divide(3, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("You should not divide a number by zero or zero by a number! " + e);
+        } finally {
+            System.out.println(result);
+        }
     }
 }
+
+
