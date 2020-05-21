563608d95dd2117fa7a7facb8623c57b59252bbf
gothamloop <g.otham@webtunnelo.com>
2020-05-21 19:40:39 +0200

    First Commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,26 @@
 package com.kodilla.exception.test;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class ExceptionHandling {
+
+    public static void main(String args[]) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+        String score = "";
+        try {
+            score = secondChallenge.probablyIWillThrowException(5.1, 4.9);
+        } catch (Exception e) {
+            System.out.println("Java exception: " + e);
+            ;
+        } finally {
+            System.out.println(score);
+        }
+
+    }
+
 }
