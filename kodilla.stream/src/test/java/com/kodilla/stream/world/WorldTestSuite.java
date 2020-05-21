74e75c23e313d600396e5366c5b953d70bdde747
gothamloop <g.otham@webtunnelo.com>
2020-05-21 11:41:12 +0200

    First Commit

--- kodilla.stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla.stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -3,8 +3,11 @@ package com.kodilla.stream.world;
 import com.kodilla.stream.sand.Asia;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+
+import static javafx.scene.input.KeyCode.L;
 
 public class WorldTestSuite {
     @Test
@@ -27,12 +30,17 @@ public class WorldTestSuite {
         world.addContinent(europa);
         world.addContinent(australia);
 
-        Country china = new Country("China",1438707163);
-        Country india = new Country("India",1380004385);
+        Country china = new Country("China", BigDecimal.valueOf(1438707163));
+        Country india = new Country("India", BigDecimal.valueOf(1380004385));
+        Country japan = new Country("Japan", BigDecimal.valueOf(126519062));
+        Country russia = new Country("Russia", BigDecimal.valueOf(145927633));
+        Country turkey = new Country("Turkey", BigDecimal.valueOf(84232553));
 
         asia.addCountry(china);
         asia.addCountry(india);
-
+        asia.addCountry(japan);
+        asia.addCountry(russia);
+        asia.addCountry(turkey);
 
 
 
