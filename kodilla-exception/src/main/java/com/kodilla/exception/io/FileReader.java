721f0ae56e06fd825778b7fa0f7743cc03fed168
gothamloop <g.otham@webtunnelo.com>
2020-05-21 17:05:14 +0200

    First Commit

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -7,11 +7,22 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
 
-    //    try (Stream<String> fileLines = Files.lines(Paths.get("test.txt"))) {
+        try (Stream<String> fileLines = Files.lines(Paths.get("test.txt"))) {
+            fileLines.forEach(System.out::println);
+        } catch (IOException e) {
+            throw new FileReaderException();
+        } finally {
+            System.out.println("I am gonna be here... always!");
+        }
+
+
+
+
+        /*
         try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
@@ -19,6 +30,6 @@ public class FileReader {
         } finally {
             System.out.println("I am gonna be here... always!");
         }
-
+        */
     }
 }
