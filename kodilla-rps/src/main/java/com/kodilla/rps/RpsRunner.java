39ed9a24d1b162864cdd42666f626850e29316d9
gothamloop <g.otham@webtunnelo.com>
2020-05-25 12:53:05 +0200

    First Commit

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 
 public class RpsRunner {
 
-
-
     public static void main(String[] args) {
 
 
+        String again = null;
+
         boolean end = false;
+        while(!end) {
         Scanner myObj = new Scanner(System.in);
 
         System.out.println("*** This is a game Rock, Paper, Scissors ***");
@@ -26,128 +27,127 @@ public class RpsRunner {
         System.out.println("Name: " + name);
         System.out.println("You play "  + round + " Round/s ");
 
-
         int userPoints=0;
         int computerPoints=0;
         int userResult=0;
         int computerResult=0;
         System.out.println("How to play?");
-        do {
+
+         do {
+
+                System.out.println("Choose a button '1','2' or '3'");
+                System.out.println("A button '1' - ROCK");
+                System.out.println("A button '2' - PAPER");
+                System.out.println("A button '3' - SCISSORS");
 
 
-        System.out.println("Choose a button '1','2' or '3'");
-        System.out.println("A button '1' - ROCK");
-        System.out.println("A button '2' - PAPER");
-        System.out.println("A button '3' - SCISSORS");
+                int buttonUser = myObj.nextInt();
+                if (buttonUser == 1) {
+                    System.out.println("You chose ROCK");
+                }
+                if (buttonUser == 2) {
+                    System.out.println("You chose PAPER");
+                }
+                if (buttonUser == 3) {
+                    System.out.println("You chose SCISSORS");
+                }
+
+                int min = 1;
+                int max = 3;
+                int ranComputer = (int) (Math.random() * (max - min + 1) + min);
+                if (ranComputer == 1) {
+                    System.out.println("Computer chose ROCK");
+                }
+                if (ranComputer == 2) {
+                    System.out.println("Computer chose PAPER");
+                }
+                if (ranComputer == 3) {
+                    System.out.println("Computer chose SCISSORS");
+                }
 
 
-        int buttonUser = myObj.nextInt();
-        if(buttonUser == 1){
-            System.out.println("You chose ROCK");
+                if (buttonUser == 1 && ranComputer == 1) {
+                    System.out.println("It is a draw");
+
+                    userResult += userPoints + 1;
+                    computerResult += computerPoints + 1;
+                }
+                if (buttonUser == 1 && ranComputer == 2) {
+                    System.out.println("Paper beats Rock, Computer win");
+
+                    computerResult += computerPoints + 1;
+                }
+                if (buttonUser == 1 && ranComputer == 3) {
+                    System.out.println("Rock beats Scissors, You win");
+
+                    userResult += userPoints + 1;
+                }
+
+                if (buttonUser == 2 && ranComputer == 1) {
+                    System.out.println("Paper beats Rock, You win");
+
+                    userResult += userPoints + 1;
+                }
+
+                if (buttonUser == 2 && ranComputer == 2) {
+                    System.out.println("It is a draw");
+
+                    userResult += userPoints + 1;
+                    computerResult += computerPoints + 1;
+                }
+
+                if (buttonUser == 2 && ranComputer == 3) {
+                    System.out.println("Scissors beats Paper, Computer win");
+
+                    computerResult += computerPoints + 1;
+                }
+
+                if (buttonUser == 3 && ranComputer == 1) {
+                    System.out.println("Rock beats Scissors, Computer win");
+
+                    computerResult += computerPoints + 1;
+                }
+
+                if (buttonUser == 3 && ranComputer == 2) {
+                    System.out.println("Scissors beats Paper, You win");
+
+                    userResult += userPoints + 1;
+                }
+
+                if (buttonUser == 3 && ranComputer == 3) {
+                    System.out.println("It is a draw");
+
+                    userResult += userPoints + 1;
+                    computerResult += computerPoints + 1;
+                }
+                System.out.println("\n");
+                round--;
+
+            } while (round > 0);
+            System.out.println("User points: " + userResult);
+            System.out.println("Computer points: " + computerResult);
+
+
+            if (userResult > computerResult) {
+                System.out.println("You win all game!");
+            }
+            if (userResult == computerResult) {
+                System.out.println("It is a draw!");
+            }
+            if (userResult < computerResult) {
+                System.out.println("Computer win all game :( ");
+            }
+
+            System.out.println("Would you like to continue?");
+            System.out.println("A button 'n' - Start the new game, Do you wanna stop playing?");
+            System.out.println("A button 'x' - Finish the game, Do you wanna stop playing?");
+            Scanner scan = new Scanner(System.in);
+            String s = scan.nextLine();
+            if(s.equals("x")) {
+                end = true;
+            }
         }
-        if(buttonUser == 2){
-            System.out.println("You chose PAPER");
-        }
-        if(buttonUser == 3){
-            System.out.println("You chose SCISSORS");
+
         }
 
-        int min = 1;
-        int max = 3;
-        int ranComputer = (int)(Math.random() * (max - min + 1) + min);
-        if(ranComputer == 1) {
-            System.out.println("Computer chose ROCK");
-        }
-        if(ranComputer == 2) {
-            System.out.println("Computer chose PAPER");
-        }
-        if(ranComputer == 3) {
-            System.out.println("Computer chose SCISSORS");
-        }
-
-
-
-
-        if(buttonUser == 1 && ranComputer == 1) {
-            System.out.println("It is a draw");
-
-           userResult += userPoints +1;
-           computerResult += computerPoints +1;
-        }
-        if(buttonUser == 1 && ranComputer == 2) {
-            System.out.println("Computer win");
-
-            computerResult += computerPoints +1;
-        }
-        if(buttonUser == 1 && ranComputer == 3) {
-            System.out.println("You win");
-
-            userResult += userPoints +1;
-        }
-
-        if(buttonUser == 2 && ranComputer == 1) {
-            System.out.println("You win");
-
-            userResult += userPoints +1;
-        }
-
-        if(buttonUser == 2 && ranComputer == 2) {
-            System.out.println("It is a draw");
-
-            userResult += userPoints +1;
-            computerResult += computerPoints +1;
-        }
-
-        if(buttonUser == 2 && ranComputer == 3) {
-            System.out.println("You win");
-
-            userResult += userPoints +1;
-        }
-
-        if(buttonUser == 3 && ranComputer == 1) {
-            System.out.println("Computer win");
-
-            computerResult += computerPoints +1;
-        }
-
-        if(buttonUser == 3 && ranComputer == 2) {
-            System.out.println("You win");
-
-            computerResult += computerPoints +1;
-        }
-
-        if(buttonUser == 3 && ranComputer == 3) {
-            System.out.println("It is a draw");
-
-            userResult += userPoints +1;
-            computerResult += computerPoints +1;
-        }
-            System.out.println("\n");
-        round--;
-
-        } while (round>0);
-        System.out.println("User points: " +userResult);
-        System.out.println("Computer points: " +computerResult);
-
-
-
-        if(userResult>computerResult){
-            System.out.println("You win all game!");
-        }
-        if(userResult==computerResult){
-            System.out.println("It is a draw!");
-        }
-        if(userResult<computerResult){
-            System.out.println("Computer win all game :( ");
-        }
-
-        // System.out.println("A button 'x' - Finish the game, Do you wanna stop playing?");
-        // System.out.println("A button 'n' - Start the new game, Do you wanna stop playing?");
-
-     //   while(!end) {
-
-
-
-
-    }
 }
