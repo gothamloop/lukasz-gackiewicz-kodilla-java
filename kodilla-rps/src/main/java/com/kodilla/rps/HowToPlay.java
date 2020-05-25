95d6b6c0a070daa2500579584c6ff917bc1c44aa
gothamloop <g.otham@webtunnelo.com>
2020-05-25 18:38:50 +0200

    First Commit

--- kodilla-rps/src/main/java/com/kodilla/rps/HowToPlay.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/HowToPlay.java
@@ -1,48 +1,28 @@
-package com.kodilla.rps;
+/*package com.kodilla.rps;
 
-import java.util.Random;
 import java.util.Scanner;
 
-public class RpsRunner {
-
-    public static void main(String[] args) {
+import com.kodilla.rps.RpsRunner;
 
 
-            String again = null;
+public class HowToPlay {
 
-            boolean end = false;
-
-
-                while (!end) {
-                Scanner myObj = new Scanner(System.in);
-
-                System.out.println("*** This is a game Rock, Paper, Scissors ***");
-                System.out.println("Please enter your name:");
-                // String input
-                String name = myObj.nextLine();
-                System.out.println("How many rounds do you wanna play?");
-                // Numerical input
-                int round = myObj.nextInt();
-
-                // Output input by user
-                System.out.println("Name: " + name);
-                System.out.println("You play " + round + " Round/s ");
-
-                int userPoints = 0;
-                int computerPoints = 0;
-                int userResult = 0;
-                int computerResult = 0;
-                System.out.println("How to play?");
+    public int UserComputer(int buttonUser,int ranComputer) {
 
 
-                do {
+
+
+
+
+
+       do {
                     Scanner myObj2 = new Scanner(System.in);
                     System.out.println("Choose a button '1','2' or '3'");
                     System.out.println("A button '1' - ROCK");
                     System.out.println("A button '2' - PAPER");
                     System.out.println("A button '3' - SCISSORS");
 
-                    int buttonUser = myObj2.nextInt();
+                    buttonUser = myObj2.nextInt();
                     if (buttonUser == 1) {
                         System.out.println("You chose ROCK");
                     }
@@ -55,7 +35,7 @@ public class RpsRunner {
 
                     int min = 1;
                     int max = 3;
-                    int ranComputer = (int) (Math.random() * (max - min + 1) + min);
+                    ranComputer = (int) (Math.random() * (max - min + 1) + min);
                     if (ranComputer == 1) {
                         System.out.println("Computer chose ROCK");
                     }
@@ -125,32 +105,16 @@ public class RpsRunner {
 
                 } while (round > 0);
 
-                System.out.println("User points: " + userResult);
-                System.out.println("Computer points: " + computerResult);
 
 
-                if (userResult > computerResult) {
-                    System.out.println("You win all game!");
-                }
-                if (userResult == computerResult) {
-                    System.out.println("It is a draw!");
-                }
-                if (userResult < computerResult) {
-                    System.out.println("Computer win all game :( ");
-                }
+    }
 
-                System.out.println("Would you like to continue?");
-                System.out.println("A button 'n' - Start the new game, Do you wanna stop playing?");
-                System.out.println("A button 'x' - Finish the game, Do you wanna stop playing?");
-                Scanner scan = new Scanner(System.in);
-                String s = scan.nextLine();
-                if (s.equals("x")) {
-                    end = true;
-                }
-            }
 
-        }
+
+
+
 
 
 
 }
+*/
