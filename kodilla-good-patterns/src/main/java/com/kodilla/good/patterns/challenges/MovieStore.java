7e202c6f902c2890fa52d82cbeb7097c1ff41c48
gothamloop <g.otham@webtunnelo.com>
2020-06-24 13:16:33 +0200

    First Commit

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 class MovieStore {
 
@@ -37,9 +38,12 @@ class MovieStore {
 class ShowAllFilms {
     public static void main(String[] args) {
 
-
         System.out.println("Showing all films:");
-        System.out.println(MovieStore.getMovies());
-
+   //   System.out.println(MovieStore.getMovies());
+   //   MovieStore.getMovies().entrySet().forEach(System.out::println);
+   //   MovieStore.getMovies().entrySet().stream().forEach(e-> System.out.println(e));
+        MovieStore.getMovies().entrySet()
+                .stream()
+                .forEach(entry -> System.out.println(entry.getKey() + " " + entry.getValue().stream().collect(Collectors.joining(" ! "))));
         }
     }
