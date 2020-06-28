3ab04750867c955e26d7db66b987445aaae9afdd
gothamloop <g.otham@webtunnelo.com>
2020-06-28 11:59:10 +0200

    Commit First

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/serwis_zamowien_produktow/ComputerOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/serwis_zamowien_produktow/ComputerOrderService.java
@@ -1,7 +1,5 @@
 package com.kodilla.good.patterns.challenges.serwis_zamowien_produktow;
 
-import java.time.LocalDateTime;
-
 public class ComputerOrderService implements ProductOrderService{
 
     @Override
