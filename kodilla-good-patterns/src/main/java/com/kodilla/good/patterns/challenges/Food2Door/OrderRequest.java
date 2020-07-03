950a3076b7038c6f9f932a4108ff5c5dd4b073f1
gothamloop <g.otham@webtunnelo.com>
2020-07-03 20:47:45 +0200

    Commit First

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/OrderRequest.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Food2Door/OrderRequest.java
@@ -1,10 +1,16 @@
 package com.kodilla.good.patterns.challenges.Food2Door;
 
+import java.time.LocalDateTime;
+
 public class OrderRequest {
     private UserOrder userOrder;
+    private ItemOrder itemOrder;
+    LocalDateTime orderTime;
 
-    public OrderRequest(UserOrder userOrder) {
+    public OrderRequest(UserOrder userOrder, ItemOrder itemOrder, LocalDateTime orderTime) {
         this.userOrder = userOrder;
+        this.itemOrder = itemOrder;
+        this.orderTime = orderTime;
     }
 
     public UserOrder getUserOrder() {
