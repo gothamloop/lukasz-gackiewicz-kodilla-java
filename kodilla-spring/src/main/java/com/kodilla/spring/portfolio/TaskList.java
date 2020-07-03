954479d4f2d9ba25e941da6dfdada3608bc55373
gothamloop <g.otham@webtunnelo.com>
2020-07-03 14:52:53 +0200

    Commit First

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -1,11 +1,13 @@
 package com.kodilla.spring.portfolio;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TaskList {
 
-    List<String> tasks;
-    tasks = new ArrayList<>();
+    private List<String> tasks;
 
-
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
 }
