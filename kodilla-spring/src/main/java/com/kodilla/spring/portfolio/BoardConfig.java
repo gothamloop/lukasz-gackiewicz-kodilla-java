a88fb9e8defab52fbf72acb3c6ca6d171bb0688d
gothamloop <g.otham@webtunnelo.com>
2020-07-06 16:45:31 +0200

    First Commit

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -2,6 +2,7 @@ package com.kodilla.spring.portfolio;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 
 @Configuration
@@ -11,15 +12,20 @@ public class BoardConfig {
     public Board getBoard() {
         return new Board(getToDoList(), getInProgressList(), getDoneList());
     }
-
+    @Bean
+    @Scope("prototype")
     public TaskList getToDoList() {
         return new TaskList();
     }
 
+    @Bean
+    @Scope("prototype")
     public TaskList getInProgressList() {
         return new TaskList();
     }
 
+    @Bean
+    @Scope("prototype")
     public TaskList getDoneList() {
         return new TaskList();
     }
