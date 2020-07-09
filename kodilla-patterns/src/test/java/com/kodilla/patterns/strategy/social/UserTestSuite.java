6d989709e16c42542cb8fe17eace8e60f32bbbce
gothamloop <g.otham@webtunnelo.com>
2020-07-09 19:20:00 +0200

    First Commit

--- kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -5,43 +5,42 @@ import org.junit.Assert;
 import org.junit.Test;
 
 
-    public class UserTestSuite {
-        @Test
-        public void testDefaultSharingStrategies() {
-            //Given
-            User steven = new Millenials("Steven Links");
-            User john = new YGeneration("John Hemerald");
-            User kodilla = new ZGeneration("Kodilla");
+public class UserTestSuite {
+    @Test
+    public void testDefaultSharingStrategies() {
 
-            //When
-            String stevenIsUser = steven.sharePost();
-            System.out.println("Steven is a " + stevenIsUser);
-            String johnIsUser = john.sharePost();
-            System.out.println("John is a " + johnIsUser);
-            String kodillaIsUser = kodilla.sharePost();
-            System.out.println("Kodilla is a " + kodillaIsUser);
+        //Given
+        User steven = new Millenials("Steven Links");
+        User john = new YGeneration("John Hemerald");
+        User kodilla = new ZGeneration("Kodilla");
 
-            //Then
-            Assert.assertEquals("Facebook Publisher", stevenIsUser);
-            Assert.assertEquals("Snapchat Publisher", johnIsUser);
-            Assert.assertEquals("Twitter Publisher", kodillaIsUser);
+        //When
+        String stevenIsUser = steven.sharePost();
+        System.out.println("Steven is a " + stevenIsUser);
+        String johnIsUser = john.sharePost();
+        System.out.println("John is a " + johnIsUser);
+        String kodillaIsUser = kodilla.sharePost();
+        System.out.println("Kodilla is a " + kodillaIsUser);
+
+        //Then
+        Assert.assertEquals("Facebook Publisher", stevenIsUser);
+        Assert.assertEquals("Snapchat Publisher", johnIsUser);
+        Assert.assertEquals("Twitter Publisher", kodillaIsUser);
         }
 
-        @Test
-        public void testIndividualSharingStrategy() {
-            //Given
-            User steven = new Millenials("Steven Links");
+    @Test
+    public void testIndividualSharingStrategy() {
+        //Given
+        User steven = new Millenials("Steven Links");
 
-            //When
-            String stevenIsUser = steven.sharePost();
-            System.out.println("Steven is a " + stevenIsUser);
-            steven.setPublisher(new TwitterPublisher());
-            stevenIsUser = steven.sharePost();
-            System.out.println("Steven should be a " + stevenIsUser);
+        //When
+        String stevenIsUser = steven.sharePost();
+        System.out.println("Steven is a " + stevenIsUser);
+        steven.setPublisher(new TwitterPublisher());
+        stevenIsUser = steven.sharePost();
+        System.out.println("Steven should be a " + stevenIsUser);
 
-            //Then
-            Assert.assertEquals("Twitter Publisher", stevenIsUser);
+        //Then
+        Assert.assertEquals("Twitter Publisher", stevenIsUser);
         }
-
-
-    }
+}
