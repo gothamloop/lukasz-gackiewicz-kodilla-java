76afc6866f22f8f12a87bee578c5dbd4359cc116
gothamloop <g.otham@webtunnelo.com>
2020-07-13 11:04:19 +0200

    First Commit

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/FindYourFlight.java
@@ -0,0 +1,22 @@
+package com.kodilla.good.patterns.challenges.FlightCompany;
+
+public class FindYourFlight {
+
+
+
+    // 1. Znalezienie wszystkich lotów z podanego miasta
+    public void findAllFlightsFromACityProvided(){}
+
+
+    // 2. Znalezienie wszystkich lotów do danego miasta
+    public void findAllFlightsToACityProvided(){}
+
+
+    // 3. Znalezienie lotów poprzez inne miasto np. lot z Gdańska przez Kraków do Wrocławia
+    public void findAllFlightByACityProvided(){}
+
+
+
+
+
+}
