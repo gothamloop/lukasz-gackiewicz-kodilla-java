b7919417864955929fb88d407fe3fbd51db7aa19
gothamloop <g.otham@webtunnelo.com>
2020-07-13 16:15:16 +0200

    First Commit

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/FindYourFlight.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/FindYourFlight.java
@@ -1,17 +1,69 @@
 package com.kodilla.good.patterns.challenges.FlightCompany;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class FindYourFlight {
 
 
-
-    // 1. Znalezienie wszystkich lotów z podanego miasta
+   // 1. Znalezienie wszystkich lotów z podanego miasta
     public static void findAllFlightsFromACityProvided(){
 
         AllFlights.getAllFlightsMap().entrySet().stream()
-                .filter(flight -> Flight.getFromAirport().equals("Warsaw"))
+              //  .filter(flight -> Flight.getFromAirport().equals("Warsaw"))
                 .forEach(System.out::println);
     }
 
+
+    public static void findAllConnectionFromOneCityVer2() {
+        AllFlights.getAllFlightsMap().entrySet().stream()
+                .map(entry -> entry.getValue())
+                .filter(city -> Flight.getFromAirport().equals("Warsaw"))
+                .forEach(System.out::println);
+
+    }
+
+
+
+
+
+
+
+   /* public static void findAllConnectionFromOneCityList() {
+        AllFlights.getAllFlightsMap().stream().filter(c -> c.getFromAirport().equals("Warsaw"))
+                .forEach(System.out::println);
+    }
+
+
+   public static void findAllConnectionFromOneCity(){
+
+       AllFlights.getAllFlightsMap().entrySet().stream()
+               .filter(flight -> Flight.getFromAirport().equals("Warsaw"))
+               .forEach(System.out::println);
+   }
+
+
+*/
+    /*
+    public static void filterFlights(Map<Integer, String> allFlightsMap){
+        Map<Integer, String> filteredFlightsMap =
+                allFlightsMap.entrySet()
+                        .stream()
+                        .filter(s -> Flight.getFromAirport().equals("Warsaw"))
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+    }
+
+
+
+
+    public static void searchFlightsFrom(List<Flight> allFlightsMap, String fromAirport) {
+        allFlightsMap.stream()
+                .filter(flight -> flight.getFromAirport().equals(fromAirport))
+                .forEach(System.out::println);
+    }
+*/
 
     // 2. Znalezienie wszystkich lotów do danego miasta
     public static void findAllFlightsToACityProvided(){
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/MainService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/MainService.java
@@ -5,12 +5,14 @@ public class MainService {
     public static void main(String[] args) {
 
 
-        FindYourFlight.findAllFlightsFromACityProvided();
-
-        FindYourFlight.findAllFlightsToACityProvided();
-
-        FindYourFlight.findAllFlightByACityProvided();
+     //   FindYourFlight.findAllFlightsFromACityProvided();
+        FindYourFlight.findAllConnectionFromOneCityVer2();
 
 
+     //    FindYourFlight.filterFlights();
+     //    FindYourFlight.findAllFlightsToACityProvided();
+     //    FindYourFlight.findAllFlightByACityProvided();
+     //    FindYourFlight.searchFlightsFrom(allFlightsMap,"Warsaw");
+     //    FindYourFlight.findAllConnectionFromOneCity();
     }
 }
