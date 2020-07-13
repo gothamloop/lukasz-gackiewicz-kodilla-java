a908803f44b8854000615911fd247a2c4a5a8d1d
gothamloop <g.otham@webtunnelo.com>
2020-07-13 09:43:57 +0200

    First Commit

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/AllFlights.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/AllFlights.java
@@ -7,23 +7,26 @@ public class AllFlights {
 
         public static Map<Integer, Flight> getAllFlightsMap() {
             Map<Integer, Flight> allFlightsMap = new HashMap<Integer, Flight>();
-            allFlightsMap.put(0,new Flight("Gdansk", "Warsaw"));
-            allFlightsMap.put(1,new Flight("Gdansk", "Crakow"));
-            allFlightsMap.put(2,new Flight("Gdansk", "Wroclaw"));
-            allFlightsMap.put(3,new Flight("Gdansk", "Rzeszow"));
-            allFlightsMap.put(4,new Flight("Warsaw", "Gdansk"));
-            allFlightsMap.put(5,new Flight("Warsaw", "Szczecin"));
-            allFlightsMap.put(6,new Flight("Warsaw", "Poznan"));
-            allFlightsMap.put(7,new Flight("Warsaw", "Wroclaw"));
-            allFlightsMap.put(8,new Flight("Warsaw", "Crakow"));
-            allFlightsMap.put(9,new Flight("Warsaw", "Rzeszow"));
-            allFlightsMap.put(10,new Flight("Crakow", "Gdansk"));
-            allFlightsMap.put(11,new Flight("Crakow", "Warsaw"));
-            allFlightsMap.put(12,new Flight("Wroclaw", "Warsaw"));
-            allFlightsMap.put(13,new Flight("Wroclaw", "Gdansk"));
-            allFlightsMap.put(14,new Flight("Rzeszow", "Warsaw"));
-            allFlightsMap.put(15,new Flight("Rzeszow", "Gdansk"));
-            allFlightsMap.put(16,new Flight("Rzeszow", "Szczecin"));
+            allFlightsMap.put(0,new Flight("Gdansk","", "Warsaw"));
+            allFlightsMap.put(1,new Flight("Gdansk","Warsaw", "Crakow"));
+            allFlightsMap.put(2,new Flight("Gdansk", "","Wroclaw"));
+            allFlightsMap.put(3,new Flight("Gdansk", "Warsaw","Rzeszow"));
+            allFlightsMap.put(4,new Flight("Warsaw","", "Gdansk"));
+            allFlightsMap.put(5,new Flight("Warsaw", "Gdansk","Szczecin"));
+            allFlightsMap.put(6,new Flight("Warsaw", "","Poznan"));
+            allFlightsMap.put(7,new Flight("Warsaw", "","Wroclaw"));
+            allFlightsMap.put(8,new Flight("Warsaw", "","Crakow"));
+            allFlightsMap.put(9,new Flight("Warsaw", "","Rzeszow"));
+            allFlightsMap.put(10,new Flight("Crakow", "Warsaw","Gdansk"));
+            allFlightsMap.put(11,new Flight("Crakow", "","Warsaw"));
+            allFlightsMap.put(12,new Flight("Wroclaw","", "Warsaw"));
+            allFlightsMap.put(13,new Flight("Wroclaw","Warsaw", "Gdansk"));
+            allFlightsMap.put(14,new Flight("Rzeszow","", "Warsaw"));
+            allFlightsMap.put(15,new Flight("Rzeszow","Warsaw", "Gdansk"));
+            allFlightsMap.put(16,new Flight("Rzeszow","", "Szczecin"));
+            allFlightsMap.put(17,new Flight("Poznan", "","Warsaw"));
+            allFlightsMap.put(18,new Flight("Szczecin", "Gdansk","Warsaw"));
+            allFlightsMap.put(19,new Flight("Szczecin","", "Rzeszow"));
 
             return allFlightsMap;
         }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/Flight.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/Flight.java
@@ -2,19 +2,22 @@ package com.kodilla.good.patterns.challenges.FlightCompany;
 
 public class Flight {
 
-      private String depAirport;
-      private String arrAirport;
+      private String fromAirport;
+      private String byAirport;
+      private String toAirport;
 
-    public Flight(String depAirport, String arrAirport) {
-        this.depAirport = depAirport;
-        this.arrAirport = arrAirport;
+    public Flight(String fromAirport, String byAirport, String toAirport) {
+        this.fromAirport = fromAirport;
+        this.byAirport = byAirport;
+        this.toAirport = toAirport;
     }
 
     public String getDepAirport() {
-        return depAirport;
+        return fromAirport;
+    }
+    public String getToAirport() { return byAirport; }
+    public String getArrAirport() {
+        return toAirport;
     }
 
-    public String getArrAirport() {
-        return arrAirport;
-    }
 }
