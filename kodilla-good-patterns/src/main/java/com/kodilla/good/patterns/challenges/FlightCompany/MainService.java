3448c4293ba63628a703ccd437cf903fabd8cb6d
gothamloop <g.otham@webtunnelo.com>
2020-07-13 16:24:21 +0200

    First Commit

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/MainService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/FlightCompany/MainService.java
@@ -5,7 +5,7 @@ public class MainService {
     public static void main(String[] args) {
 
 
-     //   FindYourFlight.findAllFlightsFromACityProvided();
+        FindYourFlight.findAllFlightsFromACityProvided();
         FindYourFlight.findAllConnectionFromOneCityVer2();
         FindYourFlight.findAllFlights();
 
