b9a4231f8baccd7c1703afbbd6616271c0503536
gothamloop <g.otham@webtunnelo.com>
2020-12-09 21:30:32 +0100

    First Commit

---
+++ kodilla-spring-web/src/test/java/com/example7/demo7/Demo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example7.demo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
